467b9287c33832658f8f1f20f7f3eccc733fe048
javergarav <javergarav@unal.edu.co>
2013-04-22 21:04:25 +0000

    + Constructor que recibe las dos fechas, en la clase Intervalo. + testIntervalosIguales.

--- tddTaller/src/main/java/org/colswe/junitlab/modelo/Intervalo.java
+++ tddTaller/src/main/java/org/colswe/junitlab/modelo/Intervalo.java
@@ -6,15 +6,23 @@ public class Intervalo {
 	private Date desde;
 	private Date hasta;
 	
+	public Intervalo(Date date1, Date date2){
+		desde = date1;
+		hasta = date2;
+	}
+	
 	public Date getDesde() {
 		return desde;
 	}
+	
 	public void setDesde(Date desde) {
 		this.desde = desde;
 	}
+	
 	public Date getHasta() {
 		return hasta;
 	}
+	
 	public void setHasta(Date hasta) {
 		this.hasta = hasta;
 	}
